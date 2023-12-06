6ff667ce63795b8f774bba3d1393f7e4cdf321fe
Ka5pyr <ka5pyr@protonmail.com>
2023-12-06 16:54:53 -0600

    Added Main Menu for program

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Aji Security is a basic OSINT Recon tool created in Python.
 The tools is a work in progress and currently a proof of concept for instructional purposes.
 
+The tool name comes from an ancient game called Go. The term __aji__ refers to moves that create potential.
+
 ## Installation
 ```bash
 git clone https://github.com/Ka5pyr/aji-sec.git
@@ -25,3 +27,13 @@ python3 aji.py
 ## Usage
 This tools is for security and educational purposes only.
 Use it at your own risk.
+
+## To-Do List
+### Tasks
+- [ ] Add more sysarg support 
+- [ ] Create a menu for choosing different discovery techniques 
+- [ ] Add SQL backend to save data into
+- [ ] Rename Option_1 function name
+
+### In Progress
+- [ ] Add more sites to the Username search section
--- aji.py
+++ aji.py
@@ -1,17 +1,57 @@
 #!/usr/local/bin/python3
 
-from username_checker import UserCheck
+from uc import UserCheck
+import signal
+import sys
 
-def main():
+def signal_handler(sig, frame):
+    print("\nCtrl+C Detected. Exiting Gracefully.")
+    # Clean Processes securely here before exiting.
+    # This will be a future update
+    sys.exit(0)
 
-    username = input("Username: ")
+
+# Main Menu User will see when ran Interactively
+def main_menu():
+    print("Welcome to the Aji Security OSINT Tool")
+    print("Choose which of the below tools you want to use")
+    print("1. Username Checker")
+
+
+# Need to Change the Function Name
+def option_1():
+    username = input("Username to Query: ")
     #username = "aji"
     if username == "":
         username = "aji"
-  
 
     checker = UserCheck(username)
     checker.username_check()
+
+# Main Function for Python Program
+def main():
+    # Catch CTRL-C and handle securely
+    signal.signal(signal.SIGINT, signal_handler)    
+
+    # Handle errors in Main Menu
+    try:
+        while True:
+            main_menu()
+            choice = input("Enter your choice ('q' to quit): ")
+
+            if choice == '1':
+                option_1()
+
+            elif choice.lower() == 'q':
+                print("Goodbye")
+                sys.exit(0)
+            
+            else:
+                print("Invalid choice. Please choose one of the options or 'q' to quit.")
+
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt attempted.")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
